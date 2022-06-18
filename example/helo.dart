810d47afef0a70bfcc14b635d96d18e4a44a4ba3
fira721 <nmaripin721@gmail.com>
2022-06-18 08:27:38 +0700

    tambah file

---
+++ example/helo.dart
@@ -0,0 +1,5 @@
+import 'package:belajar_dart_library/helo.dart';
+
+void main(List<String> args) {
+  print(sayHello('Arif'));
+}
---
+++ lib/helo.dart
@@ -0,0 +1,6 @@
+library belajar_dart_library;
+
+export 'src/say_hello.dart';
+export 'src/customer.dart' show Category, Products, Customer;
+// jika ingin method atau class tertentu saja yang di export gunakan show lalu apa yang mau di tambahkan
+// setelah melakukan export kita bisa meng akses nya contoh di directori example dart setelah di export
